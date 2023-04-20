119fcd3f719f8cd8cb51303109950b975a3297a0
Minako-Avaxia <126289670+Minako-Avaxia@users.noreply.github.com>
2023-04-20 10:06:45 -0600

    Add files via upload
    first init

---
+++ chart.py
@@ -0,0 +1,13 @@
+import altair as alt
+import pandas as pd
+import streamlit as st
+from vega_datasets import data
+
+# We use @st.cache_data to keep the dataset in cache
+@st.cache_data
+def get_data():
+    source = data.stocks()
+    source = source[source.date.gt("2004-01-01")]
+    return source
+
+source = get_data()
